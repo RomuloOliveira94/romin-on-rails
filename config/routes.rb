a4388d2a938292c2a075d3211e33496ba9b01b1a
Rômulo Oliveira <romuloffall@gmail.com>
2024-11-28 14:29:01 -0300

    #implement navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   root "posts#index"
   resources :posts
+  get "about", to: "about#index"
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
