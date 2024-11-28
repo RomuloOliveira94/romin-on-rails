33c14925a2c6f1539294ee80b131a9cf9eafbfef
Rômulo Oliveira <romuloffall@gmail.com>
2024-11-28 15:39:08 -0300

    #add translations

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,9 @@ module RominOnRails
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    config.i18n.default_locale = :'pt-BR'
+    config.i18n.available_locales = [ :'pt-BR' ]
+    config.time_zone = "America/Sao_Paulo"
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
